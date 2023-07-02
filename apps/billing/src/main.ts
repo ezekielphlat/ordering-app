5c8f39292a3104b1415eeaf96aeb959d2b74d94d
Ezekiel Abodesegun <eabodesegun@essolo.com>
2023-07-02 23:32:36 +0100

    fix biling config

--- apps/billing/src/main.ts
+++ apps/billing/src/main.ts
@@ -5,7 +5,7 @@ import { RmqService } from '@app/common/rmq/rmq.service';
 async function bootstrap() {
   const app = await NestFactory.create(BillingModule);
   const rmqService = app.get<RmqService>(RmqService);
-  app.connectMicroservice(rmqService.getOptions('BILING'));
+  app.connectMicroservice(rmqService.getOptions('BILLING'));
   await app.startAllMicroservices();
 }
 bootstrap();
